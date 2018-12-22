a4638918f11fcd0bf81886cae5ad5381d145bfeb
Tesfa Bizuneh <41780689+tbizuneh@user.noreply.github.com>
2018-12-21 22:42:35 -0500

    initial commit

---
+++ app/data/friend.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friends = [
+	{
+		name: 'Robyn Rihanna Fenty',
+		photo: 'http://celebritysuccessschool.com/wp-content/uploads/2018/03/Rihanna-1024x640.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'John William Ferrelly',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Will_Ferrell_2013.jpg/220px-Will_Ferrell_2013.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Hannah Dakota Fanning',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMjAwNjM3NjY5MF5BMl5BanBnXkFtZTcwMjM4NTYwOQ@@._V1_.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Laura Jeanne Reese Witherspoon',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Reese_Witherspoon_2011%2C_2.jpg/220px-Reese_Witherspoon_2011%2C_2.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'William Bradley Pitt',
+		photo: 'https://assets.smoothradio.com/2010/01/brad-pitt-4-1262701828-view-2.jpgg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'James Paul McCartney',
+		photo: 'https://www.thefamouspeople.com/profiles/images/paul-mccartney-4.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Mary Elle Fanning',
+		photo: 'https://parade.com/wp-content/uploads/2018/05/ElleFanning-FTR.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Angelina Jolie',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Angelina_Jolie_2_June_2014_%28cropped%29.jpg/220px-Angelina_Jolie_2_June_2014_%28cropped%29.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Miley Cyrus',
+		photo: 'https://cdn.newsapi.com.au/image/v1/b33d7ac0f13c5ef9c69c66e014d95718',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friends;
